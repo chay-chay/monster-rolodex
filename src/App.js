10e9545a7c9c0cfab86ef276d0960e0e2aff7366
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-05-16 15:51:41 -0400

    added componentDidmount

--- src/App.js
+++ src/App.js
@@ -19,6 +19,11 @@ class App extends Component {
     ]
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+    .then(users => console.log(users))
+  }
   render() {
     return (
       <div className='App'>
