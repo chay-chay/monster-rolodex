c138636226880ec45ad819f31c891e9559325d07
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-05-23 20:25:14 -0400

    Adding styles for card and set query for monster.id & size

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import './card.styles.css'
 export const Card = (props) => {
   // console.log(props)
   return (
-    <div>
+      <div className='card-container'>
+        <img alt='monster' src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
       <h1>{props.monster.name}</h1>
     </div>
   );
