f3fdb176bf3a42dc8530c6233364e26cdec1dae9
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-05-23 20:12:26 -0400

    Passing props to Card component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
-import React, { Component } from 'react'
-import './App.css';
-import { CardList } from './components/card-list/card-list.component'
+import React, { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
 class App extends Component {
   state = {
-    monsters: []
-  }
+    monsters: [],
+  };
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ monsters: users }))
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
     // .then(users => console.log(users))
   }
-  
 
   render() {
     return (
-      <div className='App'>
-        <CardList name="hi">
-        {
-          this.state.monsters.map(monster => <h1 key={monster.id}>{ monster.name}</h1>)
-        }
-        </CardList>
-        
-        
-        </div>
-    )
+      <div className="App">
+        <CardList monsters={this.state.monsters} />
+
+      </div>
+    );
   }
 }
 
